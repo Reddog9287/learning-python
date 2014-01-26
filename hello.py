61600e92653dd18c1eb9a4788eadb1cc4d398bc6
Alex Vallorosi <reddog9287@aim.com>
2014-01-25 20:02:02 -0500

    learning functions

--- hello.py
+++ hello.py
@@ -3,10 +3,12 @@ import urllib2
 import ast
 
 print "Testing"
+def makeUrlReq(url):
+    """ url is a url as a string """
+    response = urllib2.urlopen(url)
+    json = response.read()
+    return json
 
-response = urllib2.urlopen('http://api.wunderground.com/api/a4e6a83d1d8bb393/conditions/q/CA/San_Francisco.json')
-json = response.read()
-out = ast.literal_eval(json)
-
+out = ast.literal_eval(makeUrlReq("http://api.wunderground.com/api/a4e6a83d1d8bb393/conditions/q/CA/San_Francisco.json"))
 
 print out["current_observation"]
